9c9d0aa250cadf715071ecf770be781a5e773954
nate-4000 <nathanieltebo8@gmail.com>
2023-03-29 19:41:31 -0700

    wee

--- filedecode.py
+++ filedecode.py
@@ -1,5 +1,6 @@
 import tiktoken
 import struct
+import time
 
 # Load the encoding for the model
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
@@ -19,4 +20,5 @@ with open(input_file, "rb") as f_in:
     decoded_input = encoding.decode(unpacked_input)
     
     # Print the decoded input
-    print(decoded_input)
+    with open("out_%d.bin" % int(time.time()), "w") as f_out:
+        f_out.write(decoded_input)
