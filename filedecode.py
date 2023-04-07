a7ee1c69593090be71926ffff0677bd8f46c5478
nate-4000 <nathanieltebo8@gmail.com>
2023-04-06 21:32:38 -0700

    changed to little endian

--- filedecode.py
+++ filedecode.py
@@ -14,7 +14,7 @@ with open(input_file, "rb") as f_in:
     packed_input = f_in.read()
     
     # Unpack the binary data into a list of integers
-    unpacked_input = struct.unpack('>' + 'I'*(len(packed_input)//4), packed_input)
+    unpacked_input = struct.unpack('<' + 'I'*(len(packed_input)//4), packed_input)
     
     # Decode the list of integers into a string of text
     decoded_input = encoding.decode(unpacked_input)
--- fileencode.py
+++ fileencode.py
@@ -1,16 +1,12 @@
 import tiktoken
-import time
 import struct
 
 # Load the encoding for the model
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
-# Get the current Unix time
-unix_time = int(time.time())
-
 # Define the input and output file paths
 input_file = input("file to encode?")
-output_file = "encoded_file_{}.gpten".format(unix_time)
+output_file = "e%s.gpten" % input_file
 
 # Encode the input file and write the output to the new file
 with open(input_file, "r") as f_in, open(output_file, "wb") as f_out:
@@ -18,7 +14,7 @@ with open(input_file, "r") as f_in, open(output_file, "wb") as f_out:
     encoded_input = encoding.encode(f_in.read())
     
     # Convert the encoded input to a bytes object
-    packed_input = struct.pack('>' + 'I'*len(encoded_input), *encoded_input)
+    packed_input = struct.pack('<' + 'I'*len(encoded_input), *encoded_input)
     
     # Write the encoded input as binary data to the output file
     f_out.write(packed_input)
